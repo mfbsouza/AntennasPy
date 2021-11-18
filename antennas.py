0965d5935111a76813a59494e41ce0347fa9d678
mfbsouza <mfbsouza.it@gmail.com>
2021-11-18 20:20:22 -0300

    linear antenna class otimization/code cleaning

--- antennas.py
+++ antennas.py
@@ -12,27 +12,28 @@ class Linear_Antenna:
 		self.a = radius * self.lbd
 		self.L = self.lbd * lenght_factor
 		self.volt = source_voltage
-		self.delta = None
-		self.N = None
 		
 		# constants
 		self.j = 0 + 1j
 		self.e = 8.854187817e-12
 
-	def __Triangular_Base(self, z):
-		if abs(z) <= self.delta:
-			return (1 - abs(z)/self.delta)
+	# triangular base function to aproximate the current's distribution in the antenna
+	def __T(self, z_prime):
+		if abs(z_prime) <= self.delta:
+			return (1 - abs(z_prime)/self.delta)
 		else:
 			return 0
 	
-	def __z(self, n):
+	# returns the z position (like L/2) for a given n
+	def __Zn(self, n):
 		return (-self.L/2 + n * self.delta)
 
-	def __Iz(self, I_array, z, base_function=None):
-		i = 0.0 + 0.0j
+	# calculate the current value for a given z position
+	def __Iz(self, z_prime):
+		iz = 0.0 + 0.0j
 		for n in range(self.N):
-			i = i + I_array[n,0] * base_function(z - self.__z(n+1))
-		return i
+			iz = iz + self.I[n,0] * self.__T(z_prime - self.__Zn(n+1))
+		return iz
 	
 	def __G(self, z):
 		return (np.exp(-self.j*self.k*np.sqrt(z**2 + self.a**2))/(4*pi*np.sqrt(z**2 + self.a**2)))
@@ -41,7 +42,7 @@ class Linear_Antenna:
 		if m == n:
 			return ((1/(2*pi*self.delta))*np.log(self.delta/self.a) - self.j*self.k/(4*pi))
 		else:
-			return (self.__G(self.__z(m) - self.__z(n)))
+			return (self.__G(self.__Zn(m) - self.__Zn(n)))
 
 	def __A(self, m, n):
 		return (self.delta**2 * self.__psi(m,n))
@@ -49,74 +50,87 @@ class Linear_Antenna:
 	def __phi(self, m, n):
 		return ( self.__psi(m-0.5,n-0.5) - self.__psi(m-0.5,n+0.5) - self.__psi(m+0.5,n-0.5) + self.__psi(m+0.5,n+0.5) )
 		
-	def compute_mom(self, N_list, max_interactions=50):
-		currents_list = []
-		impedances_list = []
-		z_list = []
+	def compute_mom(self, N):
+		self.N = N
+		self.delta = self.L/(self.N+1)
+		excitation_point = round((self.N+1)/2 - 1)
 
-		for inter in range(max_interactions):
-			self.N = 2*inter + 1
-			self.delta = self.L/(self.N+1)
-			Z = np.zeros( (self.N, self.N), dtype=complex )
-			V = np.zeros( (self.N, 1), dtype=complex )
+		Z = np.zeros( (self.N, self.N), dtype=complex )
+		V = np.zeros( (self.N, 1), dtype=complex )
 
-			# populate voltage matrix
-			V[round(((self.N+1)/2)-1), 0] = -self.j * self.w * self.e * self.volt
+		# populate voltage matrix
+		V[excitation_point, 0] = -self.j * self.w * self.e * self.volt
 
-			# populate impedance matrix
-			for m in range(self.N):
-				for n in range(self.N):
-					Z[m,n] = self.k**2 * self.__A(m,n) - self.__phi(m,n)
+		# populate impedance matrix
+		for m in range(self.N):
+			for n in range(self.N):
+				Z[m,n] = self.k**2 * self.__A(m,n) - self.__phi(m,n)
 
-			# solve the linear system
-			I = np.linalg.solve(Z, V)
+		# solve the linear system
+		self.I = np.linalg.solve(Z, V)
 
-			# store currents, impedances and z values by a given N
-			if self.N in N_list:
+		# current distribution in the antenna
+		current_distribution = list()
+		for n in range(self.N + 2):
+			current = self.__Iz(self.__Zn(n))
+			current_distribution.append(current)
 
-				# build current list
-				currents = []
-				for i in range(self.N + 2):
-					current = self.__Iz(I, self.__z(i), self.__Triangular_Base)
-					currents.append(abs(current)*1000)
-				currents_list.append(currents)
+		# input impedance: Z = V/I
+		input_impedance = self.volt/self.I[excitation_point, 0]
 
-				# build z/lambda list
-				z = []
-				for i in range(self.N + 2):
-					z.append(self.__z(i))
-				z_list.append(z)
+		# Zn list
+		zn = list()
+		for n in range(self.N + 2):
+			zn.append(self.__Zn(n))
 
-				#build impedance list
-				input_impedance = 1/I[round(((self.N+1)/2)-1),0]
-				impedances_list.append(input_impedance)
-
-		return [currents_list, impedances_list, z_list]
-
+		return [current_distribution, input_impedance, zn]
+		
 def demo():
 	import matplotlib.pylab as plt
-	max_inter = 10
-	Ns_to_save = [1,3,5,7,9,11,13,15,19]
-	antena = Linear_Antenna()
-	current, impedance, z = antena.compute_mom(Ns_to_save, max_interactions=max_inter)
+
+	Max_N = 19
+	inter = (Max_N - 1)/2 + 1
+	antenna = Linear_Antenna()
 
 	# plotting values of the last term (N = 19)
+	current, impedance, zn = antenna.compute_mom(Max_N)
+	print("input impedance: ")
+	print(impedance, end="\n\n")
+	print("max current:")
+	print(current[round((Max_N+1)/2 - 1)], end=" A\n")
+
+	abs_current = list()
+	for element in current:
+		abs_current.append(abs(element)*1000) # convert to mA
+	print(abs_current[round((Max_N+1)/2 - 1)], end=" mA\n\n")
+
 	_, ax = plt.subplots()
-	print(impedance[-1])
-	lbl = "N = " + str(Ns_to_save[-1])
-	ax.plot(z[-1],current[-1],label=lbl)
+	ax.plot(zn, abs_current, label='N=19')
 	ax.set(ylabel='|I| mA', xlabel='Z/λ')
 	ax.grid()
 	plt.legend()
 	plt.show()
 
 	# plotting values for all Ns
+	abs_currents = list()
+	impedances = list()
+	zns = list()
+	print("impedances:")
+	for n in range(round(inter)):
+		current, impedance, zn = antenna.compute_mom(2*n+1)
+		zns.append(zn)
+		print(impedance, end=" N = ")
+		print(2*n+1)
+		impedances.append(impedance)
+		abs_current = list()
+		for element in current:
+			abs_current.append(abs(element)*1000) # convert to mA
+		abs_currents.append(abs_current)
+	
 	_, ax = plt.subplots()
-	print("")
-	for idx in range(len(Ns_to_save)):
-		print(impedance[idx])
-		lbl = "N = " + str(Ns_to_save[idx])
-		ax.plot(z[idx],current[idx], label=lbl)
+	for idx in range(len(abs_currents)):
+		legend = "N = " + str(2*idx+1)
+		ax.plot(zns[idx], abs_currents[idx], label=legend)
 	ax.set(ylabel='|I| mA', xlabel='Z/λ')
 	ax.grid()
 	plt.legend()
@@ -124,13 +138,17 @@ def demo():
 
 	# plotting impedance change per N
 	_, ax = plt.subplots()
-	resistances = []
-	reactances = []
-	for number in impedance:
+	resistances = list()
+	reactances = list()
+	Ns = list()
+	idx = 0
+	for number in impedances:
 		resistances.append(np.real(number))
 		reactances.append(np.imag(number))
-	ax.plot(Ns_to_save, resistances, label='resistance')
-	ax.plot(Ns_to_save, reactances, label='reactance')
+		Ns.append(2*idx+1)
+		idx = idx + 1
+	ax.plot(Ns, resistances, label='resistance')
+	ax.plot(Ns, reactances, label='reactance')
 	ax.set(xlabel='N')
 	ax.grid()
 	plt.legend()
